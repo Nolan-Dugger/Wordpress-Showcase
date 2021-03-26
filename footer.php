608529797f0ea62c00c4f1a79fdd47b006f53912
Nolan <judersi12@gmail.com>
2021-03-26 02:48:57 -0700

    second commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _s
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
+				?>
+			</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,51 @@
+<div class="simple-article-header">
+  <!-- Article Published Date & Reading Time -->
+  <p class="article-date-read">Published on: 26 March, 2021 - 2 min read</p>
+  
+  <!-- Article Title -->
+  <h4 class="article-title">
+      10 Fun Facts You Should Know About Nolan Dugger!
+  </h4>
+
+  <!-- Article Author Name & Comment Hyperlink -->
+  <p class="article-author-comments">
+    <em>by <a href="https://github.com/nolan-dugger">Nolan Dugger</a></em> 
+</p>
+
+  <!-- Article Social Links -->
+  <div class="article-social">
+    <a href="#" class="button social facebook">
+      <i class="fa fa-facebook fa-lg" aria-hidden="true"></i>
+    </a>
+    <a href="#" class="button social twitter">
+      <i class="fa fa-twitter fa-lg" aria-hidden="true"></i>
+    </a>
+    <a href="#" class="button social linkedin">
+      <i class="fa fa-linkedin fa-lg" aria-hidden="true"></i>
+    </a>
+    <a href="#" class="button social google-plus">
+      <i class="fa fa-google-plus fa-lg" aria-hidden="true"></i>
+    </a>
+  </div>
+
+  <!-- Article Image -->
+  <div class="article-post-image">
+    <div class="thumbnail">
+      <img src="https://i.imgur.com/22ue2CP.jpg">
+    </div>
+  </div>
+
+  <!-- Article Post Content -->
+  <div class="article-post-content">
+    <ul><li><strong>Nolan Dugger's</strong> tears can cure cancer. It's too bad he's never cried.</li>
+    <li>When the boogeyman goes to bed, he checks under his bed for <strong>Nolan Dugger</strong>.</li>
+    <li><strong>Nolan Dugger</strong> sleeps with his pillow under his gun.</li>
+    <li>Death once had a near-<strong>Nolan Dugger</strong> experience.</li>
+    <li><strong>Nolan Dugger</strong> can slam a revolving door.</li>
+    <li><strong>Nolan Dugger</strong> doesn't dial the wrong number, you answered the wrong phone.</li>
+    <li><strong>Nolan Dugger</strong> can gargle peanut butter</li>
+    <li>Helen Keller's favorite color is <strong>Nolan Dugger</strong></li>
+    <li><strong>Nolan Dugger</strong> won American Idol using only sign language</li>
+    <li><strong>Nolan Dugger</strong> was once bit by a cobra, and after 5 days of agonizing pain, the cobra died.</li></ul>
+  </div>
+</div>
